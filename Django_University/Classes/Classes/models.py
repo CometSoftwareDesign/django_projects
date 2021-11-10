5d4ba13fe4cd2c93233390650b718b46e5055477
Henry Lee <63754132+CometSoftwareDesign@users.noreply.github.com>
2021-11-09 20:59:39 -0500

    step 410

---
+++ Django_University/Classes/Classes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class djangoClasses(models.Model):
+    Title = models.CharField(max_length=100)
+    Course_Number = models.IntegerField()
+    Instructor_Name = models.CharField(max_length=50)
+    Duration = models.FloatField()
+    objects = models.Manager()
